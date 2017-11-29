c51b37cc6a759b58de0448ef3c26653774d3322e
winrar-rar <34109003+winrar-rar@users.noreply.github.com>
2017-11-29 22:22:03 +0100

    Add files via upload

---
+++ FizzBuzz/src/FizzBuzz.java
@@ -0,0 +1,31 @@
+
+/**
+ * Java version of the game FizzBuzz.
+ * 
+ * @author Christian Andersson
+ *
+ */
+public class FizzBuzz {
+	public static void main(String[] args) {
+		String fizz = "Fizz"; //3
+		String buzz = "Buzz"; //5
+		String str = "";
+		
+		for(int i = 1; i <= 100; i++) {
+			str = ""; //makes str empty each time.
+						
+			if(i % 3 == 0) { //if i can be divided by 3 then Fizz shall be printed
+				str += fizz; //adds Fizz to the String.
+			}
+			if(i % 5 == 0) { //if i can be divided by 5 then Buzz shall be printed
+				str += buzz; //adds Buzz to the String.
+			}
+			if(str == "") { //if the String is empty then i cannot be divided by 3 nor 5.
+				str += i; //adds i to the String.
+			}
+			
+			System.out.println(str); //print the String. 
+			
+		}
+	}
+}
